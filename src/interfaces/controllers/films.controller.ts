b6a6521a38b81a8c15a8a853f6c40b8ac61552c6
CesarQuint <pedrocesarquintero001@gmail.com>
2025-05-07 16:41:05 -0600

    Add documentation to films controller

--- src/interfaces/controllers/films.controller.ts
+++ src/interfaces/controllers/films.controller.ts
@@ -16,53 +16,106 @@ import { Roles } from '../decorators/roles.decorator';
 import { UserRoles } from 'src/infraestructure/schemas/user.schema';
 import { CreateFilmDto } from '../dto/create-film.request.dto';
 import { UpdateFilmDto } from '../dto/update-film.request.dto';
-import { Request } from 'express';
 
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
+import { FilmResponseDto } from '../dto/get-all-films.response.dto';
+import { ValidationErrorResponseDto } from '../dto/validation-error.response.dto';
+
+@ApiTags('Films')
 @UseGuards(RolesGuard)
 @Controller('/films')
 export class FilmsController {
   constructor(private readonly filmService: FilmsService) {}
 
+  @ApiOperation({ summary: 'Retrieve all films' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all films',
+    type: FilmResponseDto,
+    isArray: true,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not films found.',
+  })
   @Get('')
   @Roles(UserRoles.ADMIN, UserRoles.REGULAR)
-  async getAllFilms() {
+  async getAllFilms(): Promise<FilmResponseDto[]> {
     return await this.filmService.getAllFilmsData();
   }
 
-  @HttpCode(201)
+  @ApiOperation({ summary: 'Retrieve a specific film by ID' })
+  @ApiParam({ name: 'film_id', description: 'ID of the film to retrieve' })
+  @ApiResponse({
+    status: 200,
+    description: 'Film Data',
+    type: FilmResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Film not found',
+  })
+  @HttpCode(200)
   @Get('/:film_id')
   @Roles(UserRoles.REGULAR)
-  async getFilm(@Param('film_id') filmId: string) {
+  async getFilm(@Param('film_id') filmId: string): Promise<FilmResponseDto> {
     return await this.filmService.getFilmData(filmId);
   }
 
-  @HttpCode(200)
+  @ApiOperation({ summary: 'Create a new film' })
+  @ApiResponse({
+    status: 201,
+    description: 'Film successfully created',
+    type: FilmResponseDto,
+  })
+  @ApiResponse({
+    status: 422,
+    description: "There's already a film for that chapter.",
+  })
+  @ApiBadRequestResponse({
+    description: 'Validation error',
+    type: ValidationErrorResponseDto,
+  })
+  @HttpCode(201)
   @Post('')
   @Roles(UserRoles.ADMIN)
-  async createFilm(@Body() createFilmDto: CreateFilmDto) {
+  async createFilm(
+    @Body() createFilmDto: CreateFilmDto,
+  ): Promise<FilmResponseDto> {
     return await this.filmService.createFilm(createFilmDto);
   }
 
+  @ApiOperation({ summary: 'Update a film by ID' })
+  @ApiParam({ name: 'film_id', description: 'ID of the film to update' })
+  @ApiResponse({
+    status: 200,
+    description: 'Film successfully updated',
+    type: FilmResponseDto,
+  })
+  @ApiResponse({ status: 404, description: 'Film not found' })
   @HttpCode(200)
   @Put('/:film_id')
   @Roles(UserRoles.ADMIN)
   async updateFilm(
     @Param('film_id') filmId: string,
-    @Req()
-    request: Request,
     @Body() updateFilmDto: UpdateFilmDto,
   ) {
     return await this.filmService.updateFilm(filmId, updateFilmDto);
   }
 
+  @ApiOperation({ summary: 'Delete a film by ID' })
+  @ApiParam({ name: 'film_id', description: 'ID of the film to delete' })
+  @ApiResponse({ status: 204 })
   @HttpCode(204)
   @Delete('/:film_id')
   @Roles(UserRoles.ADMIN)
-  async deleteFilm(
-    @Req()
-    request: Request,
-    @Param('film_id') filmId: string,
-  ) {
+  async deleteFilm(@Param('film_id') filmId: string): Promise<void> {
     await this.filmService.deleteFilm(filmId);
   }
 }
